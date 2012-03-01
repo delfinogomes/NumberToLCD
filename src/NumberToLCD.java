7308944fb1a6b2a5c8b39a1131fd9349a00b951d
Delfino Filho <delfinogomes@gmail.com>
2012-03-01 18:34:12 -0300

    Implemented printing the numbers one, two, three and eight in the LCD

--- src/NumberToLCD.java
+++ src/NumberToLCD.java
@@ -1,4 +1,15 @@
 
 public class NumberToLCD {
 
+	public static String giveDiplay(int i) {
+		if (i == 1)
+			return "   \n  |\n  |";
+		else if( i == 2)
+			return " _ \n _|\n|_ ";
+		else if ( i == 3)
+			return " _ \n _|\n _|";
+		else
+			return " _ \n|_|\n|_|";
+	}
+
 }
--- test/NumberToLCDTest.java
+++ test/NumberToLCDTest.java
@@ -5,8 +5,46 @@ import org.junit.Test;
 public class NumberToLCDTest {
 
 	@Test
-	public void shoudFail(){
-		Assert.assertEquals(1, 2);
+	public void shoudReturnOneOnTheDisplay(){
+		String one =	"   \n"
+				+	"  |\n"
+				+	"  |";
+		System.out.println(NumberToLCD.giveDiplay(1));
+		Assert.assertEquals(NumberToLCD.giveDiplay(1), one);
+		
 	}
+	
+	@Test
+	public void shoudReturnTwoOnTheDisplay(){
+		String two = 	" _ \n"
+					+	" _|\n"
+					+	"|_ ";
+		System.out.println(NumberToLCD.giveDiplay(2));
+		Assert.assertEquals(NumberToLCD.giveDiplay(2), two);
+		
+	}
+	
+	@Test
+	public void shoudReturnThreeOnTheDisplay(){
+		String three = 	" _ \n"
+					+	" _|\n"
+					+	" _|";
+		System.out.println(NumberToLCD.giveDiplay(3));
+		Assert.assertEquals(NumberToLCD.giveDiplay(3), three);
+		
+	}
+
+	
+	@Test
+	public void shoudReturnEightOnTheDisplay(){
+		String eight = 	" _ \n"
+					+	"|_|\n"
+					+	"|_|";
+		System.out.println(NumberToLCD.giveDiplay(8));
+		Assert.assertEquals(NumberToLCD.giveDiplay(8), eight);
+		
+	}
+	
+
 	
 }
